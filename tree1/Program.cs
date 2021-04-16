9c8ada41250ed4ca9ac51cc2750d8e18f70bf4ae
psi9 <semyon.pavlenko@gmail.com>
2021-04-16 17:47:00 +1000

    Add project files.

---
+++ tree1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace tree1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = "C:\\1111";
+            Tree.PrintTree(folder, 1);
+        }
+    }
+}
---
+++ tree1/Tree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace tree1
+{
+    class Tree
+    {
+        public static void PrintTree(string folder, int maxDepth, string nesting = "", int depth = 0) {
+            if (depth >= maxDepth)
+            {
+                return;
+            }
+            DirectoryInfo directoryInfo = new DirectoryInfo(folder);
+            IOrderedEnumerable<FileSystemInfo> entries = directoryInfo.GetFileSystemInfos()
+                .OrderBy(x => x.Name);
+            //отрисовка всех элементов, кроме последнего
+            foreach (FileSystemInfo entry in entries.SkipLast(1))
+            {
+                if (entry.Attributes != FileAttributes.Directory)
+                {
+                    FileInfo fileInfo = new FileInfo(entry.FullName);
+                    Console.WriteLine("{0}├───{1} ({2})", nesting, entry.Name, fileInfo.Length);
+                }
+                else
+                {
+                    Console.WriteLine("{0}├───{1}", nesting, entry.Name);
+                    PrintTree(entry.FullName, maxDepth, String.Concat("|   ", nesting), depth++);
+                }
+            }
+            //отрисовка последнего элемента
+            FileSystemInfo lastEntry = entries.LastOrDefault();
+            if (lastEntry.Attributes != FileAttributes.Directory)
+            {
+                FileInfo fileInfo = new FileInfo(lastEntry.FullName);
+                Console.WriteLine("{0}└───{1} ({2})", nesting, lastEntry.Name, fileInfo.Length);
+            }
+            else
+            {
+                Console.WriteLine("{0}└───{1}", nesting, lastEntry.Name);
+            }
+        }
+    }
+}
